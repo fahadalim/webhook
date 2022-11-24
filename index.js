3cdd7aa85d98f879b8c061583abadb860ea22fe4
fahadalim <mohammadfahadalim@gmail.com>
2022-11-24 13:30:43 +0530

    adding userexisten"

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post("/fahad", async (req, res) => {
 // });
 
 
-app.post("/webhook", (req, res) => {
+app.post("/webhook", async (req, res) => {
   //i want some
 let userStatus = "";
   let body_param = req.body;
@@ -85,7 +85,7 @@ let userStatus = "";
       let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
       let type = body_param.entry[0].changes[0].value.messages[0].type;
 
-      const checkNewUser = User.findOne({from:from})
+      const checkNewUser = await User.findOne({from:from})
       console.log("fahad:",checkNewUser)
       if(checkNewUser === null){
         userStatus = "new"
