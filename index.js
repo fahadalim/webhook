6e4924d3b37fdcb4b836ca950b93b01e50164254
fahadalim <mohammadfahadalim@gmail.com>
2022-11-24 13:58:18 +0530

    adding userexisten\bf

--- index.js
+++ index.js
@@ -79,28 +79,31 @@ let userStatus = "";
       body_param.entry[0].changes[0].value.messages &&
       body_param.entry[0].changes[0].value.messages[0]
     ) {
-      let phon_no_id =
-        body_param.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = body_param.entry[0].changes[0].value.messages[0].from;
-      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-      let type = body_param.entry[0].changes[0].value.messages[0].type;
+        if(body_param.entry[0].changes[0].value.messages[0].type == "text"){
+            let phon_no_id =
+                body_param.entry[0].changes[0].value.metadata.phone_number_id;
+            let from = body_param.entry[0].changes[0].value.messages[0].from;
+            let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+            let type = body_param.entry[0].changes[0].value.messages[0].type;
 
-      const checkNewUser = await User.findOne({from:from})
-      console.log("fahad:",checkNewUser)
-      if(checkNewUser === null){
-        userStatus = "new"
-      }else if(checkNewUser !== null){
-        userStatus = "old"
-      }
-      let user = User.create({
-        from: from,
-        to: phon_no_id,
-        status: 0,
-        type: type,
-        body: msg_body,
-        userStatus : userStatus
-      });
-      res.status(200).send({ data: user, message: "success" });
+            const checkNewUser = await User.findOne({from:from})
+            console.log("fahad:",checkNewUser)
+            if(checkNewUser === null){
+                userStatus = "new"
+            }else if(checkNewUser !== null){
+                userStatus = "old"
+            }
+            let user = User.create({
+                from: from,
+                to: phon_no_id,
+                status: 0,
+                type: type,
+                body: msg_body,
+                userStatus : userStatus
+            });
+            res.status(200).send({ data: user, message: "success" });
+        }
+      
 
       console.log("phone number " + phon_no_id);
       console.log("from " + from);
