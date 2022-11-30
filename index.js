c2fc6649c7d96eba0243566ab48e81d6ed3a57e4
fahadalim <mohammadfahadalim@gmail.com>
2022-11-30 15:55:59 +0530

    inter

--- index.js
+++ index.js
@@ -103,8 +103,9 @@ let userStatus = "";
             });
             res.status(200).send({ data: user, message: "success" });
         }
+        
         else if(body_param.entry[0].changes[0].value.messages[0].type == "interactive"){
-          console.log(body_param.entry[0].changes[0].value.messages[0].type.interactive)
+          console.log(body_param.entry[0].changes[0].value.messages[0].type)
           if(body_param.entry[0].changes[0].value.messages[0].type.interactive.button_reply.title =="vegetable"){
             let user = User.create({
               from: from,
