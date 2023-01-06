ca8e140064254d0b7c8a2b39790db138cd760282
Mohammad Fahad Alim <mohammadfahadalim@gmail.com>
2023-01-06 14:46:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,10 +69,9 @@ app.post("/webhook", async (req, res) => {
       body_param.entry[0].changes[0].value.messages &&
       body_param.entry[0].changes[0].value.messages[0]
     ) {
-      let phon_no_id =
-            body_param.entry[0].changes[0].value.metadata.phone_number_id;
+          let phon_no_id =body_param.entry[0].changes[0].value.metadata.phone_number_id;
           let from = body_param.entry[0].changes[0].value.messages[0].from;
-          let type = body_param.entry[0].changes[0].value.messages[0].text;
+          let msg_body = body_param.entry[0].changes[0].value.messages[0].text;
         console.log("phone number " + phon_no_id);
         console.log("from " + from);
         console.log("boady param " + msg_body);
